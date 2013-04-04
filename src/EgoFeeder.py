d8bb3e612e494fea69eb160665a934d28435f088
eM <whoknowwhat0623@gmail.com>
2013-04-05 00:48:39 +0900

    Implement base model completly

--- src/EgoFeeder.py
+++ src/EgoFeeder.py
@@ -1,14 +1,29 @@
+#! -*- coding=utf-8 -*-
+
 '''
 Created on 2013. 4. 3.
 
 @author: eM <whoknowwhat0623@gmail.com>
 '''
 
-#! -*- coding=utf-8 -*-
-
 import win32api
 import win32con
 import win32gui
+
+from PIL import Image
+from PIL import ImageGrab
+
+import cv2
+from cv2 import cv
+
+import numpy
+import time
+import ctypes
+
+
+def sendKey(k):
+    win32api.keybd_event(k, 0,)
+    win32api.keybd_event(k, 0, win32con.KEYEVENTF_KEYUP,)
 
 
 def click(x, y):
@@ -17,29 +32,66 @@ def click(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
 
-def getWindow(name):
-    def callback(hwnd, results):
-        text = win32gui.GetWindowText(hwnd)
-        if text.find(name) >= 0:
-            results.append(hwnd)
-    windows = []
-    win32gui.EnumWindows(callback, windows)
-    if windows:
-        return windows[0]
-    else:
-        return None
+class WindowMgr():
+    """Encapsulates some calls to the winapi for window management"""
+    def __init__(self):
+        """Constructor"""
+        self._handle = None
+
+    def getHandle(self):
+        return self._handle
+
+    def find_window(self, window_name):
+        self._handle = None
+        win32gui.EnumWindows(self._window_enum_callback, window_name)
+
+    def _window_enum_callback(self, hwnd, name):
+        '''Pass to win32gui.EnumWindows() to check all the opened windows'''
+        if name in win32gui.GetWindowText(hwnd).decode('cp949'):
+            self._handle = hwnd
+
+    def set_foreground(self):
+        """put the window in the foreground"""
+        win32gui.SetActiveWindow(self._handle)
+        win32gui.SetForegroundWindow(self._handle)
 
 
-def setActive(name):
-    hwnd = getWindow(name)
-    if hwnd:
-        win32gui.SetActiveWindow(hwnd)
-        win32gui.SetForegroundWindow(hwnd)
+def getSnapshot():
+    return ImageGrab.grab()
+
+
+def getMatchLocation(target, template):
+    method = cv.CV_TM_SQDIFF_NORMED
+    result = cv2.matchTemplate(template, target, method)
+
+    # We want the minimum squared difference
+    _, _, mnLoc, _ = cv2.minMaxLoc(result)
+
+    # Draw the rectangle:
+    # Extract the coordinates of our best match
+    return mnLoc
+
+
+def PIL2CV(img):
+    return numpy.array(img.convert('RGB'))
 
 
 def main():
-    setActive("마비노기")
-    click(0, 0)
+    w = WindowMgr()
+    w.find_window(u"마비노기")
+    w.set_foreground()
+
+    time.sleep(1)
+    ahk = ctypes.cdll.AutoHotkey
+    ahk.ahktextdll(u"")
+    ahk.ahkExec(u"Send, {Enter}")
+    time.sleep(1)
+
+    targetImage = getSnapshot()
+    templateImage = Image.open("Chk.PNG")
+    x, y = getMatchLocation(PIL2CV(targetImage), PIL2CV(templateImage))
+    print x, y
+    click(x, y)
 
 
 if __name__ == '__main__':
