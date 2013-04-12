eafae9ebc47b71eb06a606d3a9d830950154b68c
eM <whoknowwhat0623@gmail.com>
2013-04-12 23:11:23 +0900

    add logging module

--- src/EgoFeeder.py
+++ src/EgoFeeder.py
@@ -18,11 +18,14 @@ import numpy
 import time
 import ctypes
 import ConfigParser
+import logging
 
 DELAY = 0.5
 NPCX, NPCY = 1000, 500
 ITEMX, ITEMY = 1550, 120
 TABX, TABY = 1640, 95
+SWITCH_KEY = "w"
+LOGGER = None
 
 CHK_IMAGE = "img/Chk.PNG"
 FOOD_IMAGE = "img/Food.PNG"
@@ -109,6 +112,7 @@ class EgoFeeder(object):
             "FeedState": FeedState(self),
             "BuyState": BuyState(self)
         }
+        self._feed_count = 0
         self._state = self.getState("CheckStatusState")
 
     def setState(self, state):
@@ -119,18 +123,41 @@ class EgoFeeder(object):
         if state_name in self._state_pool:
             return self._state_pool[state_name]
 
+    def counter(self):
+        self._feed_count += 1
+
     def run(self):
         while True:
             self._state.handle()
+            if self._state.getCount() > State.MAXLOOPCNT:
+                LOGGER.info("Stuck in %s" % (self._state.__name__))
+                LOGGER.info("Total feed count" % (self._feed_count))
+                self._state._inp.sendKey(SWITCH_KEY)
+                return
 
 
 class State(object):
+    MAXLOOPCNT = 10
+
     def __init__(self, context):
         self._context = context
         self._ims = ImageSearch()
         self._inp = InputMgr()
         self._wm = WindowMgr()
         self._wm.find_window(u"마비노기")
+        self._count = 0
+
+    def handle(self):
+        pass
+
+    def getCount(self):
+        return self._count
+
+    def count(self):
+        self._count += 1
+
+    def reset_count(self):
+        self._count = 0
 
 
 class CheckStatusState(State):
@@ -157,6 +184,9 @@ class CheckStatusState(State):
         x, y = self._ims.search(CHK_IMAGE)
         if (x, y) != (-1, -1):
             self._context.setState(self._context.getState("CheckInventoryState"))
+            self.reset_count()
+        else:
+            self.count()
 
         self._inp.sendKey("/")
         time.sleep(DELAY * 2)
@@ -178,14 +208,17 @@ class CheckInventoryState(State):
         else:
             self._inp.sendKey("/")
             time.sleep(DELAY * 2)
+            self.count()
             return
 
         self._inp.move(0, 0)
         x, y = self._ims.search(FOOD_IMAGE)
         if (x, y) != (-1, -1):
             self._context.setState(self._context.getState("FeedState"))
+            self.reset_count()
         else:
             self._context.setState(self._context.getState("BuyState"))
+            self.reset_count()
 
         self._inp.sendKey("/")
         time.sleep(DELAY * 2)
@@ -213,6 +246,10 @@ class FeedState(State):
             self._inp.click(x + 10, y + 50)
             self._inp.click(x + 100, y + 180)
             self._context.setState(self._context.getState("CheckStatusState"))
+            self._context.counter()
+            self.reset_count()
+        else:
+            self.count()
 
         self._inp.sendKey("/")
         time.sleep(DELAY * 2)
@@ -241,6 +278,7 @@ class BuyState(State):
             self._inp.click(ITEMX, ITEMY)
             self._inp.sendKey("{Ctrl Up}")
         else:
+            self.count()
             return
 
         self._inp.move(0, 0)
@@ -249,6 +287,7 @@ class BuyState(State):
             self._inp.click(x, y)
             self._context.setState(self._context.getState("FeedState"))
             time.sleep(DELAY * 2)
+            self.reset_count()
 
 
 def main():
@@ -258,6 +297,15 @@ def main():
     ITEMX, ITEMY = [int(n) for n in config.get("Location", "item").split(',')]
     NPCX, NPCY = [int(n) for n in config.get("Location", "npc").split(',')]
     DELAY = float(config.get("Etc", "delay"))
+    SWITCH_KEY = config.get("Etc", "switch")
+    LOGLEVEL = getattr(logging, config.get("Etc", "logging").upper(), None)
+    if LOGLEVEL:
+        LOGGER = logging.getLogger(__name__)
+        LOGGER.setLevel(LOGLEVEL)
+        LOGGER.basicConfig(format='%(asctime)s %(messages)s',
+                           filename="log.txt",
+                           datefmt="[%Y/%m/%d %H:%M:%S]",
+                           filemode='w')
 
     feeder = EgoFeeder()
     feeder.run()
