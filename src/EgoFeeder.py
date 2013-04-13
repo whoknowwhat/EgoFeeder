123e1114f6593c6bb61704998075585c6642f0dd
eM <whoknowwhat0623@gmail.com>
2013-04-13 18:14:24 +0900

    Rollback, delete logging func

--- src/EgoFeeder.py
+++ src/EgoFeeder.py
@@ -24,8 +24,6 @@ DELAY = 0.5
 NPCX, NPCY = 1000, 500
 ITEMX, ITEMY = 1550, 120
 TABX, TABY = 1640, 95
-SWITCH_KEY = "w"
-LOGGER = None
 
 CHK_IMAGE = "img/Chk.PNG"
 FOOD_IMAGE = "img/Food.PNG"
@@ -129,15 +127,9 @@ class EgoFeeder(object):
     def run(self):
         while True:
             self._state.handle()
-            if self._state.getCount() > State.MAXLOOPCNT:
-                LOGGER.info("Stuck in %s" % (self._state.__name__))
-                LOGGER.info("Total feed count" % (self._feed_count))
-                self._state._inp.sendKey(SWITCH_KEY)
-                return
 
 
 class State(object):
-    MAXLOOPCNT = 10
 
     def __init__(self, context):
         self._context = context
@@ -149,15 +141,6 @@ class State(object):
 
     def handle(self):
         pass
-
-    def getCount(self):
-        return self._count
-
-    def count(self):
-        self._count += 1
-
-    def reset_count(self):
-        self._count = 0
 
 
 class CheckStatusState(State):
@@ -184,9 +167,6 @@ class CheckStatusState(State):
         x, y = self._ims.search(CHK_IMAGE)
         if (x, y) != (-1, -1):
             self._context.setState(self._context.getState("CheckInventoryState"))
-            self.reset_count()
-        else:
-            self.count()
 
         self._inp.sendKey("/")
         time.sleep(DELAY * 2)
@@ -208,17 +188,14 @@ class CheckInventoryState(State):
         else:
             self._inp.sendKey("/")
             time.sleep(DELAY * 2)
-            self.count()
             return
 
         self._inp.move(0, 0)
         x, y = self._ims.search(FOOD_IMAGE)
         if (x, y) != (-1, -1):
             self._context.setState(self._context.getState("FeedState"))
-            self.reset_count()
         else:
             self._context.setState(self._context.getState("BuyState"))
-            self.reset_count()
 
         self._inp.sendKey("/")
         time.sleep(DELAY * 2)
@@ -247,9 +224,6 @@ class FeedState(State):
             self._inp.click(x + 100, y + 180)
             self._context.setState(self._context.getState("CheckStatusState"))
             self._context.counter()
-            self.reset_count()
-        else:
-            self.count()
 
         self._inp.sendKey("/")
         time.sleep(DELAY * 2)
@@ -278,7 +252,6 @@ class BuyState(State):
             self._inp.click(ITEMX, ITEMY)
             self._inp.sendKey("{Ctrl Up}")
         else:
-            self.count()
             return
 
         self._inp.move(0, 0)
@@ -287,7 +260,6 @@ class BuyState(State):
             self._inp.click(x, y)
             self._context.setState(self._context.getState("FeedState"))
             time.sleep(DELAY * 2)
-            self.reset_count()
 
 
 def main():
@@ -297,15 +269,6 @@ def main():
     ITEMX, ITEMY = [int(n) for n in config.get("Location", "item").split(',')]
     NPCX, NPCY = [int(n) for n in config.get("Location", "npc").split(',')]
     DELAY = float(config.get("Etc", "delay"))
-    SWITCH_KEY = config.get("Etc", "switch")
-    LOGLEVEL = getattr(logging, config.get("Etc", "logging").upper(), None)
-    if LOGLEVEL:
-        LOGGER = logging.getLogger(__name__)
-        LOGGER.setLevel(LOGLEVEL)
-        LOGGER.basicConfig(format='%(asctime)s %(messages)s',
-                           filename="log.txt",
-                           datefmt="[%Y/%m/%d %H:%M:%S]",
-                           filemode='w')
 
     feeder = EgoFeeder()
     feeder.run()
